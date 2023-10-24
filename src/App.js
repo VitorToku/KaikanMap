369d20e5cfc0369bf299cd523b86d6ba2f3c7a9d
VitorToku <130801252+VitorToku@users.noreply.github.com>
2023-10-24 16:41:50 -0300

    Correção para subir apenas a pasta Src

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomePage from './components/home';
+import Org from './components/home'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <HomePage/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import Logo from '../../img/logo.svg'
+import ListaPags from '../ListaPags'
+
+
+function Header() {
+    return(        
+        <div>
+            {/* <img src = {Logo} alt="Logotipo" /> */}
+            <ListaPags />
+            
+        </div>
+
+    )
+}
+
+export default Header
