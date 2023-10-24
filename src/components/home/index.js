e0d997c73d15821f86d5cae0dbb783b072e77b4e
Gabriel Coutinho Cezar <131826351+coutinhogit@users.noreply.github.com>
2023-10-24 10:53:33 -0300

    versao-w-route
    versao com routes porem sem link ainda

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -4,7 +4,8 @@ import style from '../../style.css'
 import vitao from '../../img/vitao.png'
 import logo from '../../img/logo.png'
 import Carousel from 'react-bootstrap/Carousel';
-// import ExampleCarouselImage from 'components/ExampleCarouselImage';
+import { Link } from "react-router-dom";
+
 
 
 
@@ -54,38 +55,37 @@ function HomePage() {
 
             </aside>
             <main>
-                <div id="carrosel">    
-                    <Carousel>
-                    <Carousel.Item>
-                        <img src={vitao} text="First slide" />
-                        <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src={vitao} text="Second slide" />
-                        <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src={vitao} text="Third slide" />
-                        <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                        </p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    </Carousel>
+                <div id="carouselExampleIndicators" class="carousel slide">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src={vitao} class="d-block w-100" alt="..."/>
+                        </div>
+                        <div class="carousel-item">
+                            <img src={vitao } class="d-block w-100" alt="..."/>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="..." class="d-block w-100" alt="..."/>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
 
 
             </main>
 
-
+            <section class="separar"/>
 
             <section id="sec1">
                 <div id="img"><img class=" " src={logo} alt="logo" /></div>
@@ -102,8 +102,9 @@ function HomePage() {
                 </div>
             </section>
 
-
+            
         </div>
+        
     )
 }
 
