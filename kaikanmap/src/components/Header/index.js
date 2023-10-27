d7ab206322c356be32dc6076dd2ddf8b72fb66fe
Gabriel Coutinho Cezar <131826351+coutinhogit@users.noreply.github.com>
2023-10-27 11:03:18 -0300

    27-10 version
    versao atualizada com routes e incio da pagina da "EQUIPE"

---
+++ kaikanmap/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import Logo from '../../img/logo.svg'
+import ListaPags from '../ListaPags'
+
+
+function Header() {
+    return(        
+        <>
+            <ListaPags />  
+        </>
+
+    )
+}
+
+export default Header
